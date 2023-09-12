d92b2ecd5c721d3142d59cd358ff6d56e44ff59d
Tigran Mekinyan <tmekinyan@mail.ru>
2023-09-12 10:43:22 +0200

    Add source code

---
+++ src/motor/motor.ino
@@ -0,0 +1,20 @@
+#define MOTOR_PIN 3
+ 
+void setup()
+{
+  pinMode(MOTOR_PIN, OUTPUT);
+ 
+  Serial.begin(115200);
+}
+ 
+void loop()
+{
+  Serial.println("Turning motor ON");
+  digitalWrite(MOTOR_PIN, HIGH);
+  delay(3000); // Wait three seconds
+ 
+  Serial.println("Turning motor OFF");
+  digitalWrite(MOTOR_PIN, LOW);
+  delay(3000); // Wait three seconds
+ 
+}
